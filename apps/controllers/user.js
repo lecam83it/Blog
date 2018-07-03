7742a6f0a4f5bfa13b2f460497dbf208c6078e42
BeChanh <lecam83it@gmail.com>
2018-07-04 00:08:56 +0700

    Sign In and Sign Up

--- apps/controllers/user.js
+++ apps/controllers/user.js
@@ -1,8 +1,60 @@
 var express = require('express');
+var userModel = require('../models/user');
+var { isRegister, generatePasswordByBcrypt, isMatch } = require('../helpers/user/helper');
+
 var router = express.Router();
 
-router.get('/', function(req, res){
-  
+var alertCustom = { status: 0, message: '' };
+
+router.get('/', function (req, res) {
+    alertCustom.status = 0;
+    alertCustom.message = "";
+    res.render('signup', {alertCustom});
 })
+
+router.post('/signin', function (req, res) {
+    var data = req.body;
+    userModel.findOne({ "email": data.email }, function (req, result) {
+        if (isMatch(data.password, result.password)) {
+            res.json({ "message": "Login successful" })
+        } else {
+            alertCustom.status = 1;
+            alertCustom.message = "Đăng nhập thất bại :( !";
+            res.render('signup' , {alertCustom});
+        }
+    })
+});
+
+router.post('/signup', function (req, res) {
+    var data = req.body;
+    var status = isRegister(data);
+    userModel.find({ "email": data.email }, function (req, result) {
+        if (status) {
+            if(result.length == 0){
+                var user = {
+                    "email": data.email,
+                    "password": generatePasswordByBcrypt(data.password),
+                    "first_name": data.first_name,
+                    "last_name": data.last_name,
+                    "created_at": Date.now(),
+                    "updated_at": Date.now(),
+                }
+                userModel.insertMany(user, function (req, result) {
+                    alertCustom.status = 2;
+                    alertCustom.message = "Đăng kí thành công :) !";
+                    res.render('signup' , {alertCustom});
+                });
+            }else {
+                alertCustom.status = 1;
+                alertCustom.message = "Email đã tồn tại! :(";
+                res.render('signup' , {alertCustom});
+            }
+        } else {
+            alertCustom.status = 1;
+            alertCustom.message = "Password không trùng khớp :( !";
+            res.render('signup' , {alertCustom});
+        }
+    });
+});
 
 module.exports = router;
